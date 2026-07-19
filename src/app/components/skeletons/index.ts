771d70c66b3c288597ea52ed4f45ecd6e85694be
Zyad <zyad.kamal1706859@science.menofia.edu.eg>
2026-07-19 11:24:40 +0300

    chore: remove unused imports and app duplicate folders

--- src/app/components/skeletons/index.ts
+++
@@ -1,5 +0,0 @@
-export { SkeletonBlock } from './SkeletonBlock';
-export { CardSkeleton, CardSkeletonGrid } from './CardSkeleton';
-export { VerseSkeleton, VerseSkeletonList } from './VerseSkeleton';
-export { PrayerPageSkeleton, PrayerWidgetSkeleton } from './PrayerSkeleton';
-export { AdhkarFeaturedSkeleton, AdhkarGridSkeleton, DhikrCardSkeleton } from './AdhkarSkeleton';
--- src/imports/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const ThemeContext = createContext(undefined);
-
-export default ThemeContext;
